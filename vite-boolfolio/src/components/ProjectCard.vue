53a2e1022392f9708fa3965d0e96cab041b81f6d
Anna <annadalex@icloud.com>
2024-04-09 17:34:55 +0200

    Milestone 5 terminato

--- vite-boolfolio/src/components/ProjectCard.vue
+++ vite-boolfolio/src/components/ProjectCard.vue
@@ -14,8 +14,6 @@ export default {
   }
 };
 </script>
-
-<template>
    
 
 
@@ -23,21 +21,40 @@ export default {
    <template>
   <main>
     <div class="project-card">
-      <h3>Progetto:</h3>
-    
-    </div>
-    <div class="card" style="width: 18rem;">
-      <div class="card-body">
-        <h5>{{ project.title }}</h5>
-        <p>{{ project.content }}</p>
-        <a href="#" class="btn btn-primary">Vai da qualche parte</a>
+    <h3>Progetto:</h3>
+    <div class="card-container">
+        <div class="card-body">
+          <h5 class="card-title">{{ project.title }}</h5>
+          <p class="card-text">{{ project.content }}</p>
+          <a href="#" class="btn btn-primary">Vai da qualche parte</a>
+        </div>
       </div>
     </div>
+
   </main>
 </template>
-  </template>
   
-  <style scoped>
+  
+  <style scoped >
+  .project-card {
+  margin-bottom: 20px; /* Aggiungi spazio sotto la sezione */
+}
+
+.card-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between; /* Distribuisci gli spazi tra le card */
+}
+
+.card {
+  width: calc(25% - 10px); /* Imposta la larghezza di ogni card al 25% del contenitore meno lo spazio tra le card */
+  margin-bottom: 20px; /* Aggiungi spazio tra le card */
+}
+
+.card-body {
+  text-align: center; 
+}
+
   
   </style>
   
